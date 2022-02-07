4a0b745ebb68bd5345ae45f7f4f916fc4b04d43c
Hazem <hazemzon@gmail.com>
2022-02-07 21:41:22 +0200

    configurate database ts file and env  file

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,15 @@ const address: string = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
+app.get('/', (req: Request, res: Response) : void => {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+
+
+app.listen(3000, (): void => {
     console.log(`starting app on: ${address}`)
 })
+
+
+export default app;
