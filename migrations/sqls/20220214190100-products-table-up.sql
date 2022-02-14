3f10f7a5c5bef0b0e26a5e58b19d936b54b172c6
Hazem <hazemzon@gmail.com>
2022-02-14 21:03:05 +0200

    add products migrations

---
+++ migrations/sqls/20220214190100-products-table-up.sql
@@ -0,0 +1,9 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
+    price NUMERIC(17,2) NOT NULL,
+    category VARCHAR(50) NOT NULL
+);
