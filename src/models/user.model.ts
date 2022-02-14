593f2ab5ca69abe43a050cfcc255f66a3058420e
Hazem <hazemzon@gmail.com>
2022-02-14 21:56:57 +0200

    add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,25 @@
+import User from "../types/user.types";
+import db from "../database";
+
+class UserModel {
+  //get all
+  async index(): Promise<User[]> {
+    try {
+      const connection = db.connect();
+      const sql = "SELECT * FROM users";
+      const result = await (await connection).query(sql);
+      (await connection).release();
+      return result.rows;
+    } catch (err: any) {
+      throw new Error(`Can't get users from DB${err.message}`);
+    }
+  }
+
+  //get by id1
+  //create
+  //update
+  //delete
+  //authenticate
+}
+
+export default UserModel;
