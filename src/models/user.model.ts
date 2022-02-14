cf53959f88b148837d528383b0afc85fcbb5630f
Hazem <hazemzon@gmail.com>
2022-02-14 23:12:32 +0200

    implement CRUD for users table model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -13,11 +13,78 @@ class UserModel {
       throw new Error(`Can't get users from DB${err.message}`);
     }
   }
-
   //get by id1
+  async getById(id: number): Promise<User[]> {
+    try {
+      const connection = db.connect();
+      const sql = "SELECT * FROM users WHERE id=($1)";
+      const result = await (await connection).query(sql, [id]);
+      (await connection).release();
+      return result.rows[0];
+    } catch (err: any) {
+      throw new Error(`Can't get user ${id}, ${err.message}`);
+    }
+  }
   //create
+  async createUser(user: User): Promise<User> {
+    try {
+      const connection = db.connect();
+      const sql =
+        "INSERT INTO users (first_name,last_name,password,email) values ($1, $2, $3, $4) returning id,first_name,last_name,email";
+
+      const result = await (
+        await connection
+      ).query(sql, [
+        user.first_name,
+        user.last_name,
+        user.password,
+        user.email,
+      ]);
+      (await connection).release();
+      return result.rows[0];
+    } catch (error: any) {
+      throw new Error(`Can't create users ${error.message}`);
+    }
+  }
   //update
+  async updateUser(user: User): Promise<User> {
+    try {
+      const connection = db.connect();
+      const sql =
+        "UPDATE users SET first_name=$1, last_name=$2, password=$3, email=$4 WHERE id=$5 RETURNING *";
+      const result = await (
+        await connection
+      ).query(sql, [
+        user.first_name,
+        user.last_name,
+        user.password,
+        user.email,
+        user.id,
+      ]);
+      (await connection).release();
+      return result.rows[0];
+    } catch (error: any) {
+      throw new Error(
+        `Can't Update user ${user.first_name + user.last_name} ,error is: ${
+          error.message
+        }`
+      );
+    }
+  }
   //delete
+  async deleteUser(id: number): Promise<User> {
+    try {
+      const connection = db.connect();
+      const sql = "DELETE FROM users WHERE id=($1) RETURNING *";
+      const result = await (await connection).query(sql, [id]);
+      (await connection).release();
+      return result.rows[0];
+    } catch (error: any) {
+      throw new Error(
+        `Couldn't delete user Id: ${id}, Error: ${error.message}`
+      );
+    }
+  }
   //authenticate
 }
 
