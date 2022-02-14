181444092384c6bad4a9b5235939d5f30aa94ec3
Hazem <hazemzon@gmail.com>
2022-02-14 21:08:02 +0200

    add products - orders relation table migration

---
+++ migrations/sqls/20220214190356-order-products-table-up.sql
@@ -0,0 +1,8 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE order_products(
+    id SERIAL PRIMARY KEY,
+    quantity INTEGER NOT NULL,
+    order_id BIGINT REFERENCES orders(id) NOT NULL,
+    product_id BIGINT REFERENCES products(id) NOT NULL
+);
